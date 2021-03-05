92eee3d8ed8fb1b20d3a4d1e65c43f442436c3f6
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2021-03-05 16:24:39 +0100

    Adds cron to save photos

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,16 +19,17 @@
 
 # Learn more: http://github.com/javan/whenever
 
-#every 10.minutes do
-#  response1 = HTTParty.get("https://my.tikee.io/v2/photo_sets/b5c0711e-b6f1-4d91-aa2d-55db20b47eec/last_signed_url", headers: { 
-#    "Accept" => "application/json" })
-#  data1 = response1.to_hash
-#  photo = Photo.new
-#  photo.url = data1['url']['url']['original']
-#  photo.date = data1['url']['date']
-#  photo.number = data1['count']
-#  photo.photo_set = 'b5c0711e-b6f1-4d91-aa2d-55db20b47eec'
-#  photo.camera = 'vita_1'
-#  photo.image.attach(io: photo.url, filename: photo.camera)
-#  photo.save
-#end
+every 10.minutes do
+  response1 = HTTParty.get("https://my.tikee.io/v2/photo_sets/b5c0711e-b6f1-4d91-aa2d-55db20b47eec/last_signed_url", headers: { 
+    "Accept" => "application/json" })
+  data1 = response1.to_hash
+  photo = Photo.new
+  photo.url = data1['url']['url']['original']
+  file = URI.open(photo.url)
+  photo.date = data1['url']['date']
+  photo.number = data1['count']
+  photo.photo_set = 'b5c0711e-b6f1-4d91-aa2d-55db20b47eec'
+  photo.camera = 'vita_1'
+  photo.image.attach(io: file, filename: photo.camera, content_type: 'image/jpg')
+  photo.save
+end
